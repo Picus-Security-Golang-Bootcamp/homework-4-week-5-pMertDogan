bd148971decd116e495ea2351e2be311a6b41b67
Mert <pme2012@yandex.com>
2022-04-02 00:04:20 +0300

    enableMiddleware

--- controller/controllerDefinations.go
+++ controller/controllerDefinations.go
@@ -20,38 +20,18 @@ func SetupControllers() {
 	//set handlers whitelist if needed
 	// handlers.AllowedMethods([]string{"POST", "GET", "PUT", "PATCH"})
 
-	//*******Without gorilla Mux//can be moved another file
-	//curl -v "http://localhost:8080/statusCheck"
-	http.HandleFunc("/statusCheck", StatusCheck)
-	//curl -v "http://localhost:8080/book?bookID=1"
-	http.HandleFunc("/book", bookHandler)
-	//curl -v "http://localhost:8080/bookWithAuthor?bookID=1"
-	//Maybe we can change it to /book and change the handling method use path variable like /book?author=true
-	http.HandleFunc("/bookWithAuthor", bookRest.GetBookByIdIncludeAuthor)
-	//curl -v "http://localhost:8080/book/find?name=Hob"
-	http.HandleFunc("/book/find", bookRest.FindBookByNameWithoutAuthor)
-	//curl -v "http://localhost:8080/book" POST
-	http.HandleFunc("/book/", bookRest.FindBookByNameWithoutAuthor)
-	http.HandleFunc("/book/enable", bookRest.EnableBook)
 
 	//curl -v "http://localhost:8080/statusCheck"
 	r.HandleFunc("/statusCheck", StatusCheck)
-	//curl -v "http://localhost:8080/book?bookID=1"
-	r.HandleFunc("/book", bookHandler)
-	//curl -v "http://localhost:8080/bookWithAuthor?bookID=1"
-	//Maybe we can change it to /book and change the handling method use path variable like /book?author=true
-	r.HandleFunc("/bookWithAuthor", bookRest.GetBookByIdIncludeAuthor)
-	//curl -v "http://localhost:8080/book/find?name=Hob"
-	r.HandleFunc("/book/find", bookRest.FindBookByNameWithoutAuthor)
-	//curl -v "http://localhost:8080/book" POST
-	r.HandleFunc("/book/enable", bookRest.EnableBook)
+
+	//Bookhandler is a function that will handle all the request for book
+	bookRest.BookHandler(r)
 
 	// http.HandleFunc("/book", bookRest.UpdateBookWithID) NOT IMPLEMENTED YET
 	//******Middleware
-
 	// r.Use(admin.AdminLogger)
 	//add admin middleware
-	// r.Use(admin.AdminMiddleware)
+	r.Use(admin.AdminMiddleware)
 
 	//**** Author Handles it with Gorilla Mux
 	//setup admin routes
@@ -74,36 +54,5 @@ func SetupControllers() {
 
 	// log.Println(http.ListenAndServeTLS(":8080", "certFile", "keyFile", nil))
 	log.Println(http.ListenAndServe(":8080", r))
-
-}
-
-func bookHandler(w http.ResponseWriter, r *http.Request) {
-	// var responseModel domain.APIStruct = domain.APIStruct{}
-	log.Println(r.Method)
-	w.Header().Set("Content-Type", "application/json")
-
-	//redirect to method
-	switch r.Method {
-	case http.MethodGet:
-		log.Println("get book by id")
-		bookRest.GetBookByID(w, r)
-		return
-	case http.MethodPost:
-		bookdID := r.URL.Query().Get("bookID")
-		if bookdID != "" {
-			bookRest.UpdateBookQuantity(w, r)
-		} else {
-			//somethink like
-			// bookRest.UpdateBook(w, r)
-		}
-		return
-
-	case http.MethodDelete:
-		bookRest.SoftDelete(w, r)
-		return
-	default:
-		http.Error(w, "Invalid request", http.StatusBadRequest)
-		return
-	}
 
 }
